268d1e2b7981129ee3adda1669e1a7ad3f8276ad
Leap <git@leap.new>
2025-11-07 18:41:38 +0000

    Fix microphone setup returning "HelloHello"

--- frontend/hooks/useSpeechRecognition.ts
+++ frontend/hooks/useSpeechRecognition.ts
@@ -32,7 +32,7 @@ export function useSpeechRecognition(): UseSpeechRecognitionResult {
       let interimTranscript = '';
       let finalTranscript = '';
 
-      for (let i = event.resultIndex; i < event.results.length; i++) {
+      for (let i = 0; i < event.results.length; i++) {
         const transcriptPiece = event.results[i][0].transcript;
         if (event.results[i].isFinal) {
           finalTranscript += transcriptPiece + ' ';
@@ -41,10 +41,7 @@ export function useSpeechRecognition(): UseSpeechRecognitionResult {
         }
       }
 
-      setTranscript((prev) => {
-        const newTranscript = prev + finalTranscript;
-        return newTranscript || interimTranscript;
-      });
+      setTranscript(finalTranscript || interimTranscript);
     };
 
     recognition.onerror = (event: any) => {
