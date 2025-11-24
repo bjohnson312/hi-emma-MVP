5384f77dbe2b72772e9cdc52fb4931d0ceff2048
Leap <git@leap.new>
2025-11-24 20:07:29 +0000

    Revert to V1 conversation system

--- frontend/config.ts
+++ frontend/config.ts
@@ -2,4 +2,4 @@ export const CLERK_PUBLISHABLE_KEY = "pk_test_ZXBpYy1sb3VzZS04Ny5jbGVyay5hY2NvdW
 
 export const USE_NEW_GREETING_FLOW = false;
 
-export const USE_NEW_CONVERSATION_FLOW = true;
+export const USE_NEW_CONVERSATION_FLOW = false;
